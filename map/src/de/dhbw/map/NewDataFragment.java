19fe775f9b1ffed5ff3cab72f78fe63412b8239e
Hisham ElGezeery <hisham.elgezeery@gmail.com>
2014-08-06 13:21:57 +0200

    added gauges to project

--- map/src/de/dhbw/map/NewDataFragment.java
+++ map/src/de/dhbw/map/NewDataFragment.java
@@ -1,7 +1,12 @@
 package de.dhbw.map;
 
 
+import java.util.Random;
+
+import de.dhbw.ui.GaugeView;
+
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +15,10 @@ import de.dhbw.R;
 
 
 public class NewDataFragment extends Fragment {
-
+	
+	private GaugeView mGaugeView1;
+	private GaugeView mGaugeView2;
+	private final Random RAND = new Random();
 	/**
 	 * Returns a new instance of this fragment for the given section number.
 	 */
@@ -24,8 +32,22 @@ public class NewDataFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_data_2, container,
 				false);
+		mGaugeView1 = (GaugeView) rootView.findViewById(R.id.gauge_view1);
+		mGaugeView2 = (GaugeView) rootView.findViewById(R.id.gauge_view2);
+		mTimer.start();
 		return rootView;
 	}
 	
+	private final CountDownTimer mTimer = new CountDownTimer(30000, 1000) {
+
+		@Override
+		public void onTick(final long millisUntilFinished) {
+			mGaugeView1.setTargetValue(RAND.nextInt(101));
+			mGaugeView2.setTargetValue(RAND.nextInt(101));
+		}
+
+		@Override
+		public void onFinish() {}
+	};
 	
 }
